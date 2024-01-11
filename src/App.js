c95b684dddc22343ca1983b9232eab3b846df328
onkar-giram <meonkargiram@gmail.com>
2024-01-11 19:12:12 +0530

    removed a typo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App mb-[183px]">
       <h2 className="invisible md:visible martel-sans-ud text-[#887C68] font-semibold text-[28px] mt-[183px]">
-        COMMUNITIES WE MANAGE1
+        COMMUNITIES WE MANAGE
       </h2>
       <h2 className="absolute top-[183px] left-0 right-0 visible md:invisible martel-sans-ud text-[#887C68] font-semibold text-[28px] ">
         COMMUNITIES <br />
